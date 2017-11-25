857a9c0e5a36e18562b7950ff438fdf285ebd8eb
zbig1000 <zbig.lupina@gmail.com>
2017-11-25 18:39:54 +0100

    first commit

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+public class Calculator {
+// tested class
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+    public double substract (double a, double b) {
+        return a - b;
+    }
+
+}
---
+++ src/main/java/com/kodilla/KodillaCourseApplication.java
@@ -0,0 +1,19 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.*;
+
+@SpringBootApplication
+public class KodillaCourseApplication {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(KodillaCourseApplication.class, args);
+
+
+		Calculator kalkulator = new Calculator();
+		System.out.println("dodawanie 2+2 = " + kalkulator.add(2d, 2d));
+		System.out.println("odejmowanie 4.6 - 2.1 = " + kalkulator.substract(4.6, 2.1));
+	}
+}
