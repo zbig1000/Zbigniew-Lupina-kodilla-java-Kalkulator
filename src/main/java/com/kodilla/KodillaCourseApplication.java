98f6d5548b5c126f0d1251c638cf6d193463fa9a
zbig1000 <zbig.lupina@gmail.com>
2017-12-02 18:59:33 +0100

    4.6 after restoring

--- src/main/java/com/kodilla/KodillaCourseApplication.java
+++ src/main/java/com/kodilla/KodillaCourseApplication.java
@@ -16,5 +16,6 @@ public class KodillaCourseApplication {
 		System.out.println("dodawanie 2+2 = " + kalkulator.add(2d, 2d));
 		System.out.println("odejmowanie 4.6 - 2.1 = " + kalkulator.substract(4.6, 2.1));
 		System.out.println("UHU test GITa");
+
 	}
 }
