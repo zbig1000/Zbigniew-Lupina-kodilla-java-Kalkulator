3fda2b344b271edf88289fcd87162f35f466ecf5
zbig1000 <zbig.lupina@gmail.com>
2017-12-16 17:01:55 +0100

    6.6 Lesson

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/library/LibraryUser.java
@@ -0,0 +1,54 @@
+package com.kodilla.testing.library;
+
+import java.util.Objects;
+
+public class LibraryUser {
+    String firstname;
+    String lastname;
+    String peselId;
+
+    public LibraryUser(String firstname, String lastname, String peselId) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.peselId = peselId;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getPeselId() {
+        return peselId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LibraryUser that = (LibraryUser) o;
+        return Objects.equals(firstname, that.firstname) &&
+                Objects.equals(lastname, that.lastname) &&
+                Objects.equals(peselId, that.peselId);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstname.hashCode();
+        result = 31 * result + lastname.hashCode();
+        result = 31 * result + peselId.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "LibraryUser{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", peselId='" + peselId + '\'' +
+                '}';
+    }
+}
