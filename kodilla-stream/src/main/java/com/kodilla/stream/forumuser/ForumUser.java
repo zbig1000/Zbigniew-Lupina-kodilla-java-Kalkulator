b1688103c63f15ccb6c00576ba7a8b9e9f2d3adb
zbig1000 <zbig.lupina@gmail.com>
2017-12-27 11:40:54 +0100

    7.3 Task Stream - Corrected (naming pattern)

--- kodilla-stream/src/main/java/com/kodilla/stream/forumuser/ForumUser.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/ForumUser.java
@@ -3,48 +3,48 @@ package com.kodilla.stream.forumuser;
 import java.time.LocalDate;
 
 public class ForumUser {
-    private final int user_id;
-    private final String user_name;
+    private final int userId;
+    private final String userName;
     private final char gender;
-    private final LocalDate birth_date;
-    private final int posts_num;
+    private final LocalDate birthDate;
+    private final int postsNum;
 
-    public ForumUser(final int user_id, final String user_name, final char gender, final LocalDate birth_date, final int posts_num) {
-        this.user_id = user_id;
-        this.user_name = user_name;
+    public ForumUser(final int userId, final String userName, final char gender, final LocalDate birthDate, final int postsNum) {
+        this.userId = userId;
+        this.userName = userName;
         this.gender = gender;
-        this.birth_date = birth_date;
-        this.posts_num = posts_num;
+        this.birthDate = birthDate;
+        this.postsNum = postsNum;
     }
 
-    public final int getUser_id() {
-        return user_id;
+    public final int getUserId() {
+        return userId;
     }
 
-    public final String getUser_name() {
-        return user_name;
+    public final String getUserName() {
+        return userName;
     }
 
     public final char getGender() {
         return gender;
     }
 
-    public final LocalDate getBirth_date() {
-        return birth_date;
+    public final LocalDate getBirthDate() {
+        return birthDate;
     }
 
-    public final int getPosts_num() {
-        return posts_num;
+    public final int getPostsNum() {
+        return postsNum;
     }
 
     @Override
     public String toString() {
         return "ForumUser{" +
-                "user_id=" + user_id +
-                ", user_name='" + user_name + '\'' +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
                 ", gender=" + gender +
-                ", birth_date=" + birth_date +
-                ", posts_num=" + posts_num +
+                ", birthDate=" + birthDate +
+                ", postsNum=" + postsNum +
                 '}';
     }
 }
