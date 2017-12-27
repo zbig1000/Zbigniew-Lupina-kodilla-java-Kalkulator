1fad672854c245db21fc881fb21c00ea3a295645
zbig1000 <zbig.lupina@gmail.com>
2017-12-27 15:26:56 +0100

    7.5 Task IntStream extended

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,22 @@
+package com.kodilla.stream.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.stream.IntStream;
+
+public class ArrayOperationsTestSuite {
+
+    @Test
+    public void testGetAverage() {
+        //Given
+        int[] numbers = {10,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+
+        //When
+        double average = ArrayOperations.getAverage(numbers);
+
+        //Then
+        Assert.assertEquals(10.95, average, 0.01);
+
+    }
+}
