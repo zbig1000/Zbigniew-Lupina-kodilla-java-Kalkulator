50fa75f7d17a14d6db9d831c68fe1a1b5081bbc7
zbig1000 <zbig.lupina@gmail.com>
2017-12-28 18:30:59 +0100

    8.3 Lesson Exception

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReaderWithoutHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReaderWithoutHandling.java
@@ -7,22 +7,15 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-public class FileReader {
-    public void readFile() throws FileReaderException {
+public class FileReaderWithoutHandling {
+    public void readFile() throws IOException {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("files/names.txt").getFile());
         Path path = Paths.get("files/test.txt");
 //        Path path = Paths.get(file.getPath());
-        try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath())))   {
-        fileLines.forEach(System.out::println);
-        }
-        catch (IOException e) {
-        throw new FileReaderException();
-        }
-        finally {
-            System.out.println("jestem tu zawsze");
+         Stream<String> fileLines = Files.lines(Paths.get(file.getPath()));
+         fileLines.forEach(System.out::println);
 
-        }
 
     }
 }
