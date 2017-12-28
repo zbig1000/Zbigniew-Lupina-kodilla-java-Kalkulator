f5b0696deeff186b157e2926d48bbd307bcc5273
zbig1000 <zbig.lupina@gmail.com>
2017-12-28 17:38:06 +0100

    8.2 Task Exception try() catch()

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -16,10 +16,21 @@ public class FirstChallenge {
     public static void main(String[] args) {
 
         FirstChallenge firstChallenge = new FirstChallenge();
-
-        double result = firstChallenge.divide(3, 0);
-
+        double result = 0d;
+        String message = "";
+        try { result = firstChallenge.divide(3, 0);
+        } catch (ArithmeticException e) {
+            System.out.println("Divide by ZERO forbidden " + e);
+        }
+        catch (Exception e) {
+            System.out.println("General Exception occurred " + e);
+        }
+        finally {
         System.out.println(result);
+        }
+
+
 
     }
+
 }
