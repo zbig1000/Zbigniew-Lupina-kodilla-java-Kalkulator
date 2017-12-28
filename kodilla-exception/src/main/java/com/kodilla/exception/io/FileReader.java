cd550353d86373dd7b5732246b842283431536c6
zbig1000 <zbig.lupina@gmail.com>
2017-12-28 17:16:29 +0100

    8.2 Lesson Exceptions

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -0,0 +1,28 @@
+package com.kodilla.exception.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FileReader {
+    public void readFile() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("files/names.txt").getFile());
+        Path path = Paths.get("files/test.txt");
+//        Path path = Paths.get(file.getPath());
+        try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath())))   {
+        fileLines.forEach(System.out::println);
+        }
+        catch (IOException e) {
+        System.out.println("no nie, nie ma pliku" + e);
+        }
+        finally {
+            System.out.println("jestem tu zawsze");
+
+        }
+
+    }
+}
