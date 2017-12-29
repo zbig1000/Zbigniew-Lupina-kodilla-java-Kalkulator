26a7ab58d8e64e02d7f62ebc2f7cf4be08779325
zbig1000 <zbig.lupina@gmail.com>
2017-12-29 15:36:32 +0100

    8.3 Task CORRECTED Thow & Throws

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -5,19 +5,18 @@ import com.kodilla.exception.io.FileReaderException;
 import java.util.stream.Stream;
 
 public class ExceptionHandling {
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
         SecondChallenge secondChallenge = new SecondChallenge();
         String result = null;
         try {
             result = secondChallenge.probablyIWillThrowException(2.5, 2d);
+            System.out.println("result " + result);
+        } catch (Exception e) {
+            System.out.println("Exception occurred in probablyIWillThrowException " + e);
+        } finally {
+            System.out.println("probablyIWillThrowException called");
         }
-        catch (Exception e) {
-            System.out.println("Exception occurred in probablyIWillThrowException "+e);
-        }
-        finally {
-            if (result == null) result = "Operation unsuccessful";
-        }
-        System.out.println("result "+ result);
+
     }
 }
