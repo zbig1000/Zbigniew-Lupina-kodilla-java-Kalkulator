8a103c6886f83a2d146be3f00bdc6a9cbed67e0f
zbig1000 <zbig.lupina@gmail.com>
2017-12-30 13:14:45 +0100

    9.1 Task CORRECTED Clean Code (with JUnit)

---
+++ kodilla-good-patterns/src/test/java/com/kodilla/good/patterns/challenges/MovieStoreTestSuiteTask.java
@@ -0,0 +1,52 @@
+package com.kodilla.good.patterns.challenges;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+public class MovieStoreTestSuiteTask {
+
+    public Map<String, List<String>> prepareTestData() {
+        List<String> ironManTranslations = new ArrayList<>();
+        ironManTranslations.add("Żelazny Człowiek");
+        ironManTranslations.add("Iron Man");
+
+        List<String> avengersTranslations = new ArrayList<>();
+        avengersTranslations.add("Mściciele");
+        avengersTranslations.add("Avengers");
+
+        List<String> flashTranslations = new ArrayList<>();
+        flashTranslations.add("Błyskawica");
+        flashTranslations.add("Flash");
+
+        Map<String, List<String>> moviesWithTranslations = new HashMap<>();
+        moviesWithTranslations.put("IM", ironManTranslations);
+        moviesWithTranslations.put("AV", avengersTranslations);
+        moviesWithTranslations.put("FL", flashTranslations);
+
+        return moviesWithTranslations;
+    }
+
+    @Test
+    public void testMovieStore() {
+        // Given
+        MovieStore movieStore = new MovieStore(prepareTestData());
+
+        // When
+        String movieList = movieStore.getMovies().entrySet().stream().parallel()
+                .map(entry -> entry.getValue())
+                .flatMap(list -> list.stream())
+                .collect(Collectors.joining("!", "", ""));
+
+        // Then
+        String expectedResult = "Żelazny Człowiek!Iron Man!Mściciele!Avengers!Błyskawica!Flash";
+        assertEquals(expectedResult, movieList);
+    }
+}
+
