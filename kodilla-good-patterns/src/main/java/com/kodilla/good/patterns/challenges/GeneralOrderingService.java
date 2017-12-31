7e6a85ed33fe7d59d054c0b322e34384d60f3a37
zbig1000 <zbig.lupina@gmail.com>
2017-12-31 11:26:41 +0100

    9.2 Task - Ordering Service

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/GeneralOrderingService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/GeneralOrderingService.java
@@ -3,46 +3,46 @@ package com.kodilla.good.patterns.challenges;
 import java.util.stream.Collectors;
 
 public class GeneralOrderingService {
-    private InformationServiceInterface informationServiceInterface;
-    private OrderingServiceInterface orderingServiceInterface;
-    private OrderRepositoryInterface orderRepositoryInterface;
+    private InformationServiceInterface informationService;
+    private OrderingServiceInterface orderingService;
+    private OrderRepositoryInterface orderRepository;
 
-    public GeneralOrderingService(final InformationServiceInterface informationServiceInterface,
-                                  final OrderingServiceInterface orderingServiceInterface,
-                                  final OrderRepositoryInterface orderRepositoryInterface) {
-        this.informationServiceInterface = informationServiceInterface;
-        this.orderingServiceInterface = orderingServiceInterface;
-        this.orderRepositoryInterface = orderRepositoryInterface;
+    public GeneralOrderingService(final InformationServiceInterface informationService,
+                                  final OrderingServiceInterface orderingService,
+                                  final OrderRepositoryInterface orderRepository) {
+        this.informationService = informationService;
+        this.orderingService = orderingService;
+        this.orderRepository = orderRepository;
     }
 
     public OrderDto process(final OrderRequest orderRequest) {
-        boolean isSold = orderingServiceInterface.sale(orderRequest);
+        boolean isSold = orderingService.sale(orderRequest);
 
         if (isSold) {
-            informationServiceInterface.inform(orderRequest.getUser());
-            orderRepositoryInterface.createOrder(orderRequest);
+            informationService.inform(orderRequest.getUser());
+            orderRepository.createOrder(orderRequest);
             return new OrderDto(orderRequest.getUser(), true);
         } else {
             return new OrderDto(orderRequest.getUser(), false);
         }
     }
     public String getUserOrderList(User user) {
-       return orderRepositoryInterface.getAllUserOrders(user).stream()
+       return orderRepository.getAllUserOrders(user).stream()
                 .map(order -> order.getOrderKey().getKey())
                 .map(Object::toString)
                 .collect(Collectors.joining("'", "<",">"));
     }
     public long getTotalItemOrdered() {
-        return orderRepositoryInterface.getAllExistingOrders().stream()
+        return orderRepository.getAllExistingOrders().stream()
                 .map(order -> order.getQuantity())
                 .reduce(0, (total, quantity) -> total += quantity);
     }
     public long getUserOrderQuantity(User user) {
-        return orderRepositoryInterface.getAllUserOrders(user).stream()
+        return orderRepository.getAllUserOrders(user).stream()
                 .count();
     }
     public long getTotalOrderQuantity() {
-        return orderRepositoryInterface.getAllExistingOrders().stream()
+        return orderRepository.getAllExistingOrders().stream()
         .count();
     }
 }
