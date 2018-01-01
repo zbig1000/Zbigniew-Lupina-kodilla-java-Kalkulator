42f23ef83aff4c085bb10517c19bdde7cd74e7be
zbig1000 <zbig.lupina@gmail.com>
2018-01-01 13:19:13 +0100

    9.1 Task CORRECTED Clean Code (with JUnit)

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -1,5 +1,6 @@
 package com.kodilla.good.patterns.challenges;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -8,19 +9,17 @@ public class MovieStore {
     private  Map<String, List<String>> moviesWithTranslations;
 
     public MovieStore(Map<String, List<String>> moviesWithTranslations) {
-        this.moviesWithTranslations = moviesWithTranslations;
+        this.moviesWithTranslations = new HashMap<String, List<String>>(moviesWithTranslations);
     }
 
     public Map<String, List<String>> getMovies() {
-        return moviesWithTranslations;
+        return new HashMap<String, List<String>>(moviesWithTranslations);
     }
 
     public String translationString() {
-        if (!moviesWithTranslations.isEmpty())
         return moviesWithTranslations.entrySet().stream().parallel()
                 .map(entry -> entry.getValue())
                 .flatMap(list -> list.stream())
                 .collect(Collectors.joining("!", "", ""));
-        else return "";
     }
 }
