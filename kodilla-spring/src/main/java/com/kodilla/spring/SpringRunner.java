72f85901f19e6a45920a57f31d4f089cbb5b344a
zbig1000 <zbig.lupina@gmail.com>
2018-01-06 21:57:12 +0100

    10.1 Lesson Spring Intro - DI

--- kodilla-spring/src/main/java/com/kodilla/spring/SpringRunner.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/SpringRunner.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KodillaSpringApplication {
+public class SpringRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KodillaSpringApplication.class, args);
+		SpringApplication.run(SpringRunner.class, args);
 	}
 }
