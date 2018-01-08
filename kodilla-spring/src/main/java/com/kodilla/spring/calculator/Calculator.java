f3db2e35b1f6b24eaa1e907b91dc528be88d9b63
zbig1000 <zbig.lupina@gmail.com>
2018-01-08 11:56:12 +0100

    10.2 Task - Bean Config (Calculator)

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -8,19 +8,27 @@ public class Calculator {
     @Autowired
     private Display display;
 
-    public void add(double a, double b) {
-        display.displayValue(a + b);
+    public double add(double a, double b) {
+        double result = a + b;
+        display.displayValue(result);
+        return result;
     }
 
-    public void sub(double a, double b) {
-        display.displayValue(a - b);
+    public double sub(double a, double b) {
+        double result = a - b;
+        display.displayValue(result);
+        return result;
     }
 
-    public void mul(double a, double b) {
-        display.displayValue(a * b);
+    public double mul(double a, double b) {
+        double result = a * b;
+        display.displayValue(result);
+        return result;
     }
 
-    public void div(double a, double b) {
-        display.displayValue(a / b);
+    public double div(double a, double b) {
+        double result = a / b;
+        display.displayValue(result);
+        return result;
     }
 }
--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,5 +1,6 @@
 package com.kodilla.spring.calculator;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,12 +18,18 @@ public class CalculatorTestSuite {
         ApplicationContext context =
                 new AnnotationConfigApplicationContext("com.kodilla.spring");
         Calculator calculator = context.getBean(Calculator.class);
+
         //When
-        calculator.add(0.1d, 0.2d);
-        calculator.div(0.1d, 0.2d);
-        calculator.mul(0.1d, 0.2d);
-        calculator.sub(0.1d, 0.2d);
+        double addResult = calculator.add(0.1, 0.2);
+        double subResult = calculator.sub(0.1, 0.2);
+        double mulResult = calculator.mul(0.1, 0.2);
+        double divResult = calculator.div(0.1, 0.2);
+
         //Then
-        //do nothing
+        Assert.assertEquals(0.3, addResult, 0.0001d);
+        Assert.assertEquals(-0.1, subResult, 0.0001d);
+        Assert.assertEquals(0.02, mulResult, 0.0001d);
+        Assert.assertEquals(0.5, divResult, 0.0001d);
+
     }
 }
