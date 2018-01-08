e5502bced67300f863108d0b061f6519c5d3ccaa
zbig1000 <zbig.lupina@gmail.com>
2018-01-08 11:59:25 +0100

    10.2 Task - Bean Config (Calculator)

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -3,13 +3,11 @@ package com.kodilla.spring.calculator;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class CalculatorTestSuite {
 
     @Test
