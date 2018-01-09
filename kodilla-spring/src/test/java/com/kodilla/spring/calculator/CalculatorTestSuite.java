40d4611c5394fd43985093d1d66cd519d85fc03b
zbig1000 <zbig.lupina@gmail.com>
2018-01-09 20:12:47 +0100

    10.2 Lesson - Bean Config- CORRECTED

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class CalculatorTestSuite {
 
     @Test
-    public void testCalculations() {
+    public void testAddCalculations() {
         //Given
         ApplicationContext context =
                 new AnnotationConfigApplicationContext("com.kodilla.spring");
@@ -19,14 +19,50 @@ public class CalculatorTestSuite {
 
         //When
         double addResult = calculator.add(0.1, 0.2);
-        double subResult = calculator.sub(0.1, 0.2);
-        double mulResult = calculator.mul(0.1, 0.2);
-        double divResult = calculator.div(0.1, 0.2);
 
         //Then
         Assert.assertEquals(0.3, addResult, 0.0001d);
+    }
+
+    @Test
+    public void testSubCalculations() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+
+        //When
+        double subResult = calculator.sub(0.1, 0.2);
+
+        //Then
         Assert.assertEquals(-0.1, subResult, 0.0001d);
+    }
+
+    @Test
+    public void testMulCalculations() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+
+        //When
+        double mulResult = calculator.mul(0.1, 0.2);
+
+        //Then
         Assert.assertEquals(0.02, mulResult, 0.0001d);
+    }
+
+    @Test
+    public void testDivCalculations() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+
+        //When
+        double divResult = calculator.div(0.1, 0.2);
+
+        //Then
         Assert.assertEquals(0.5, divResult, 0.0001d);
 
     }
