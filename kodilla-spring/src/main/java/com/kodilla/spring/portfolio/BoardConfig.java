8ad97bdd0a357351a9a14f5eb2a329cfed06a6f7
zbig1000 <zbig.lupina@gmail.com>
2018-01-09 20:33:34 +0100

    10.3 Task Bean config withn java class- CORRECTED

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,5 +1,6 @@
 package com.kodilla.spring.portfolio;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -7,9 +8,17 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class BoardConfig {
 
+    @Autowired
+    TaskList taskList;
+
     @Bean
-    @Scope("prototype")
     public Board getBoard() {
         return new Board();
+    }
+
+    @Bean
+    @Scope("prototype")
+    public TaskList getTaskList() {
+        return new TaskList();
     }
 }
--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardConfigTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardConfigTestSuite.java
@@ -13,26 +13,13 @@ public class BoardConfigTestSuite {
         //Given
         ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
         Board boardOne = context.getBean(Board.class);
-        Board boardTwo = context.getBean(Board.class);
-        Board boardThree = context.getBean(Board.class);
 
         boardOne.addTaskToDoList("OneToDoTask");
         boardOne.addTaskInProgressList("OneInProgressTask");
         boardOne.addTaskDoneList("OneDoneTask");
 
-        boardTwo.addTaskToDoList("TwoToDoTask");
-        boardTwo.addTaskInProgressList("TwoInProgressTask");
-        boardTwo.addTaskDoneList("TwoDoneTask");
-
-        boardThree.addTaskToDoList("ThreeToDoTask");
-        boardThree.addTaskInProgressList("ThreeInProgressTask");
-        boardThree.addTaskDoneList("ThreeDoneTask");
-
         //When
         System.out.println(boardOne.readToDoList()+" " + boardOne.readInProgressList()+" "+boardOne.readDoneList());
-        System.out.println(boardTwo.readToDoList()+" " + boardTwo.readInProgressList()+" "+boardTwo.readDoneList());
-        System.out.println(boardThree.readToDoList()+" " + boardThree.readInProgressList()+" "+boardThree.readDoneList());
-
         // & Then
     }
 }
