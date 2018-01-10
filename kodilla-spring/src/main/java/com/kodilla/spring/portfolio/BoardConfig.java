9daeea74995f3abec8b1f31a4cb93e05a638e4d7
zbig1000 <zbig.lupina@gmail.com>
2018-01-10 21:00:59 +0100

    10.3 Task Bean config withn java class- changed, NOT corrected

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,28 +1,44 @@
 package com.kodilla.spring.portfolio;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BoardConfig {
 
-    @Autowired
-    TaskList toDoList;
-    @Autowired
-    TaskList inProgressList;
-    @Autowired
-    TaskList doneList;
-
     @Bean
     public Board getBoard() {
-        return new Board(toDoList, inProgressList, doneList);
+        return new Board(getToDoList(), getInProgressList(), getDoneList());
     }
 
     @Bean
-    @Scope("prototype")
-    public TaskList getTaskList() {
+    public TaskList getToDoList() {
         return new TaskList();
     }
+    @Bean
+    public TaskList getInProgressList() {
+        return new TaskList();
+    }
+    @Bean
+    public TaskList getDoneList() {
+        return new TaskList();
+    }
+
+//    @Autowired
+//    TaskList toDoList;
+//    @Autowired
+//    TaskList inProgressList;
+//    @Autowired
+//    TaskList doneList;
+//
+//    @Bean
+//    public Board getBoard() {
+//        return new Board(toDoList, inProgressList, doneList);
+//    }
+//
+//    @Bean
+//    @Scope("prototype")
+//    public TaskList getTaskList() {
+//        return new TaskList();
+//    }
 }
