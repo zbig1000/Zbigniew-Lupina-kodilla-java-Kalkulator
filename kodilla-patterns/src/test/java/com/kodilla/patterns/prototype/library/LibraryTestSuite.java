c7a565c26bc9e91a7de810848ff9b5920ce388fa
zbig1000 <zbig.lupina@gmail.com>
2018-01-13 17:40:56 +0100

    11.2 Task - Prototype

--- kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -4,7 +4,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.time.LocalDate;
-import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
 public class LibraryTestSuite {
