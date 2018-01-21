738dd2b491f775f75be4abe0fa3f463b22fac537
zbig1000 <zbig.lupina@gmail.com>
2018-01-21 14:10:57 +0100

    12.6 Task - SQL in JAVA

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -39,4 +39,34 @@ public class DbManagerTestSuite {
         statement.close();
         Assert.assertEquals(5, counter);
     }
+
+    @Test
+    public void testSelectUsersAndPosts() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = "SELECT U.FIRSTNAME, U.LASTNAME, COUNT(*) AS POSTS_NUM\n" +
+                "FROM USERS U\n" +
+                "JOIN POSTS ON\n" +
+                "USER_ID = U.ID\n" +
+                "GROUP BY POSTS.USER_ID\n" +
+                "HAVING (POSTS_NUM > 1)\n" +
+                "ORDER BY U.ID;\n";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while(rs.next()) {
+            System.out.println(
+                    rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME") + ", " +
+                    rs.getInt("POSTS_NUM"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        Assert.assertEquals(1, counter);
+    }
 }
