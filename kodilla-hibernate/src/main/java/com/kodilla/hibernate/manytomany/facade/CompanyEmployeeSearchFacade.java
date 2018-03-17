1d6266474dbb3dfb7c2a7f0800f3ce7ec4cf3cba
zbig1000 <zbig.lupina@gmail.com>
2018-03-17 16:09:09 +0100

    20.1 Task - Facade for Hibernate

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/facade/CompanyEmployeeSearchFacade.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/facade/CompanyEmployeeSearchFacade.java
@@ -6,7 +6,6 @@ import com.kodilla.hibernate.manytomany.dao.CompanyDao;
 import com.kodilla.hibernate.manytomany.dao.EmployeeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
