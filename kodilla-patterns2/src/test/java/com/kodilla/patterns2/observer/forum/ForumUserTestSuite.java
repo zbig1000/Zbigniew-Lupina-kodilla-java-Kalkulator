8f105f4ba724d14c77adec2405e77ecbd9b84156
zbig1000 <zbig.lupina@gmail.com>
2018-03-18 18:25:05 +0100

    20.4 Lesson - Observer Forum

---
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/forum/ForumUserTestSuite.java
@@ -0,0 +1,32 @@
+package com.kodilla.patterns2.observer.forum;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class ForumUserTestSuite {
+    @Test
+    public void testUpdate() {
+        //Given
+        ForumTopic javaHelpForum = new JavaHelpForumTopic();
+        ForumTopic javaToolsForum = new JavaToolsForumTopic();
+        ForumUser johnSmith = new ForumUser("John Smith");
+        ForumUser ivoneEscobar = new ForumUser("Ivone Escobar");
+        ForumUser jessiePinkman = new ForumUser("Jessie Pinkman");
+        javaHelpForum.registerObserver(johnSmith);
+        javaHelpForum.registerObserver(jessiePinkman);
+        javaToolsForum.registerObserver(ivoneEscobar);
+        javaToolsForum.registerObserver(jessiePinkman);
+        //When
+        javaHelpForum.addPost("Hi everyone !");
+        javaHelpForum.addPost("Better try ..");
+        javaHelpForum.addPost("Why while ?");
+        javaToolsForum.addPost("Help pls");
+        javaToolsForum.addPost("When i try..");
+        //Then
+        assertEquals(3, johnSmith.getUpdateCount());
+        assertEquals(2, ivoneEscobar.getUpdateCount());
+        assertEquals(5, jessiePinkman.getUpdateCount());
+
+
+    }
+}
