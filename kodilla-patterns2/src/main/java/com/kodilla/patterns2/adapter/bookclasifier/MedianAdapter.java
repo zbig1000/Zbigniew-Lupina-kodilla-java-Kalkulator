334c35b572666c2bd7d4c883e5491fd9172fb369
zbig1000 <zbig.lupina@gmail.com>
2018-03-22 20:15:29 +0100

    20.5 Task - Adapter Library

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
@@ -1,4 +1,19 @@
 package com.kodilla.patterns2.adapter.bookclasifier;
 
-public class MedianAdapter {
+import com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book;
+import com.kodilla.patterns2.adapter.bookclasifier.librarya.Classifier;
+import com.kodilla.patterns2.adapter.bookclasifier.libraryb.BookSignature;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MedianAdapter extends MedianAdaptee implements Classifier {
+    @Override
+    public int publicationYearMedian(Set<com.kodilla.patterns2.adapter.bookclasifier.librarya.Book> bookSet) {
+        Map<BookSignature, Book> books =
+                new HashMap<>();
+        bookSet.forEach(t -> books.put(new BookSignature(t.getSignature()), new Book(t.getAuthor(), t.getTitle(), t.getPublicationYear())));
+        return medianPublicationYear(books);
+    }
 }
--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
@@ -1,10 +1,29 @@
 package com.kodilla.patterns2.adapter.bookclassifier;
 
+import com.kodilla.patterns2.adapter.bookclasifier.MedianAdapter;
+import com.kodilla.patterns2.adapter.bookclasifier.librarya.Book;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class MedianAdapterTestSuite {
     @Test
     public void publicationYearMedianTest() {
+        //Given
+        Set<Book> bookSet = new HashSet<>();
 
+        bookSet.add(new Book("jan", "number1", 2001, "lib00101"));
+        bookSet.add(new Book("john", "number2", 1999, "lib20101"));
+        bookSet.add(new Book("jura", "number3", 1993, "lib09801"));
+        bookSet.add(new Book("jura", "number4", 1995, "lib09181"));
+
+        //When
+        MedianAdapter medianAdapter = new MedianAdapter();
+        int publicationYearMedian = medianAdapter.publicationYearMedian(bookSet);
+
+        //Then
+        Assert.assertEquals(1999, publicationYearMedian);
     }
 }
