14bf1cef55d063aefd0f2e656c6ecb57aed4de94
zbig1000 <zbig.lupina@gmail.com>
2018-03-22 19:10:47 +0100

    20.5 Task - Adapter

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
@@ -1,4 +1,10 @@
 package com.kodilla.patterns2.adapter.bookclassifier;
 
+import org.junit.Test;
+
 public class MedianAdapterTestSuite {
+    @Test
+    public void publicationYearMedianTest() {
+
+    }
 }
