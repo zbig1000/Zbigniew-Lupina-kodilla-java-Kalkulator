c0b5ceda9653cd86febe62c5d7fca7ebf0de1ea2
zbig1000 <zbig.lupina@gmail.com>
2018-03-22 21:36:28 +0100

    20.5 Task - Adapter Library

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
@@ -8,12 +8,14 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-public class MedianAdapter extends MedianAdaptee implements Classifier {
+public class MedianAdapter implements Classifier {
+    MedianAdaptee medianAdaptee = new MedianAdaptee();
+
     @Override
     public int publicationYearMedian(Set<com.kodilla.patterns2.adapter.bookclasifier.librarya.Book> bookSet) {
         Map<BookSignature, Book> books =
                 new HashMap<>();
         bookSet.forEach(t -> books.put(new BookSignature(t.getSignature()), new Book(t.getAuthor(), t.getTitle(), t.getPublicationYear())));
-        return medianPublicationYear(books);
+        return medianAdaptee.medianPublicationYear(books);
     }
 }
